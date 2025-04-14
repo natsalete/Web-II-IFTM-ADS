772355dc8ecaf712d076ffa9d17bdd11a00916f2
natalete <natsalete14@gmail.com>
2025-04-14 14:01:34 -0300

    feat: adição da classe de servico e sua interface

---
+++ demo/demo/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+ 
+import com.example.demo.model.Product; 
+ 
+ public interface ProductService {
+     List <Product> getAllProducts();
+
+     void saveProduct(Product product);
+     Product getProductById(long id);
+     void deleteProductById(long id);
+ }
---
+++ demo/demo/src/main/java/com/example/demo/service/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import java.util.List;
+ import java.util.Optional;
+ 
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Service;
+ 
+ import com.example.demo.model.Product;
+ import com.example.demo.repository.ProductRepository;
+ 
+ @Service
+ public class ProductServiceImpl implements ProductService {
+ 
+     @Autowired
+     private ProductRepository productRepository;
+ 
+     @Override
+     public List <Product> getAllProducts(){
+         return productRepository.findAll();
+     }
+ 
+     @Override
+     public void saveProduct(Product product){
+         this.productRepository.save(product);
+     }
+ 
+     @Override
+     public Product getProductById(long id) {
+         Optional < Product > optional = productRepository.findById(id);
+         if (optional.isPresent()) {
+             return optional.get();
+         } else {
+             throw new RuntimeException("Product not found with id: " + id);
+         }
+     }
+ 
+     @Override
+     public void deleteProductById(long id) {
+         this.productRepository.deleteById(id);
+     }
+ 
+ }
