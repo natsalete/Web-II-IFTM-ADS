a96cc5912462956cd1aa49538d7adec135669e19
natalete <natsalete14@gmail.com>
2025-04-14 14:11:48 -0300

    feat: adicionando funcionalidade de listar todos produto

--- demo/demo/src/main/java/com/example/demo/controller/ProductController.java
+++ demo/demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,15 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
  
+ import com.example.demo.model.Product;
+ import com.example.demo.service.ProductService;
  
  @Controller
  public class ProductController {
+
+    @Autowired
+     private ProductService productService;
  
-     @GetMapping("/product")
-     public String getMethodName() {
-         return "product/index";
-     }
+    @GetMapping("/product")
+    public String index(Model model) {
+        model.addAttribute("productsList", productService.getAllProducts());
+        return "product/index";
+    }
      
  }
