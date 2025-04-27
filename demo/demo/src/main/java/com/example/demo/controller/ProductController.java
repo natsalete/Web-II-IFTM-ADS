1541d161aa0d9498e45a6d1978a6ef436aec47cd
natalete <natsalete14@gmail.com>
2025-04-27 10:56:32 -0300

    feta:Funcionalidade de cadastrar produtos

--- demo/demo/src/main/java/com/example/demo/controller/ProductController.java
+++ demo/demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import com.example.demo.model.Product;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+ import org.springframework.web.bind.annotation.PostMapping;
  
  @Controller
  public class ProductController {
@@ -19,5 +21,16 @@ import com.example.demo.service.ProductService;
         model.addAttribute("productsList", productService.getAllProducts());
         return "product/index";
     }
-     
+    
+    @GetMapping("/product/create")
+     public String create(Model model) {
+         model.addAttribute("product", new Product());
+         return "product/create";
+     }
+ 
+     @PostMapping("/product/save")
+     public String postMethodName(@ModelAttribute("product") Product product) {
+         productService.saveProduct(product);
+         return "redirect:/product";
+     }
  }
